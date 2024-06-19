7ba189c7304b90b3a9adc8f675042ce5852961ca
greenmg0102 <greenmg0102@proton.me>
2024-06-19 02:04:26 -0700

    ##

--- src/pages/poker/user/landing/Responsive.tsx
+++ src/pages/poker/user/landing/Responsive.tsx
@@ -5,11 +5,11 @@ export default function Responsive() {
     return (
         <div className="sm:flex transition-all">
             <div className="relative mx-auto mb-5 sm:mb-0 ltr:sm:mr-8 rtl:sm:ml-8 z-[2] before:absolute before:top-12 before:left-1/2 before:-bottom-[15px] before:-translate-x-1/2 before:border-l-2 before:border-[#ebedf2] before:w-0 before:h-auto before:-z-[1] dark:before:border-[#191e3a] before:hidden sm:before:block">
-                <img src="/assets/images/profile-17.jpg" alt="img" className="w-18 h-18 md:w-14 md:h-14 2xl:w-12 2xl:h-12 mx-auto rounded-full shadow-[0_4px_9px_0_rgba(31,45,61,0.31)] transition-all" />
+                <img src="/assets/images/profile-16.jpg" alt="img" className="w-18 h-18 md:w-14 md:h-14 2xl:w-12 2xl:h-12 mx-auto rounded-full shadow-[0_4px_9px_0_rgba(31,45,61,0.31)] transition-all" />
             </div>
             <div className="flex-1">
-                <h4 className="text-primary text-xl font-bold text-center ltr:sm:text-left rtl:sm:text-right">How to replay the poker hand?</h4>
-                <p className="text-center ltr:sm:text-left rtl:sm:text-right">New Brand Poker Strategy Analyzing platform with Replay function!</p>
+                <h4 className="text-primary text-xl font-bold text-center ltr:sm:text-left rtl:sm:text-right">Is it only available on the web?</h4>
+                <p className="text-center ltr:sm:text-left rtl:sm:text-right">We guarantee operation on all devices!</p>
                 <p className="text-center ltr:sm:text-left rtl:sm:text-right"></p>
                 <div className="mt-1 sm:mt-1 w-full pr-0 lg:pr-24 py-12 pb-24">
                     <div className='flex justify-center justify-between items-center flex-wrap w-full' >
